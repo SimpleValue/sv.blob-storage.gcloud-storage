33b6e2c1bd87c1d9b4b62719136cb1191ea4bdc0
Maximilian Weber <weber.maximilian@googlemail.com>
2016-06-01 07:35:45 +0200

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sv.blob-storage/gcloud-storage "0.1.0"
+(defproject sv.blob-storage/gcloud-storage "0.1.1-SNAPSHOT"
   :description "Implementation of the sv/blob-storage protocol for
                 Google Cloud Storage."
   :license {:name "Eclipse Public License"
